5a9c4b097aedfa043caa838190faf854503ea764
SEU_NOME <SEU_EMAIL>
2025-11-02 15:27:24 -0300

    Auto: atualiza aulas (2025-11-02 15:27:24)

--- aula025.py
+++ aula025.py
@@ -1,17 +1,17 @@
-a1 = 100
+a1 = 100 
 a2 = 50
 a3 = 250
 a4 = 10
 
-print(min(a1, a2, a3, a4))
-print(10 * '-')
-print(max(a1, a2, a3, a4))
+print(min(a1, a2, a3, a4)) #Valor minimo entre a1, a2, a3, a4
+print(10 * '-') # Linha Separação
+print(max(a1, a2, a3, a4)) # Valor máximo entre a1, a2, a3, a4
 print(10 * '-')  
-print(sum((a1, a2, a3, a4)))
+print(sum((a1, a2, a3, a4))) # Soma a1, a2, a3, a4
 print(10 * '-')
-print(sorted((a1, a2, a3, a4))) 
+print(sorted((a1, a2, a3, a4))) # Ordena os valores
 print(10 * '-')
-print((a1 + a2 + a3 + a4 ) // 4)
+print((a1 + a2 + a3 + a4 ) // 4) # Média inteira dos valores
 print(10 * '-')
-print((a1 + a2 + a3 + a4 ) / 4)
+print((a1 + a2 + a3 + a4 ) / 4) # Média real dos valores
 print(10 * '-')
