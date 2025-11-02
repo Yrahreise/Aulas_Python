929ffdea0afb4a31055051abaee3ad38e4f43a61
SEU_NOME <SEU_EMAIL>
2025-11-02 13:00:51 -0300

    Adiciona aulas 21 a 24

---
+++ aula023.py
@@ -0,0 +1,9 @@
+# Operador lógico "not"
+# Usado para inverter expressões booleanas
+# not True = False
+# not False = True
+#senha = input('Senha: ') # Pede para o usuário digitar uma senha
+#if not senha: # Verifica se a senha está vazia')
+print(not True) # Inverte o valor booleano True para False
+print(not False) # Inverte o valor booleano False para True
+    
---
+++ aula024.py
@@ -0,0 +1,24 @@
+# Operadores 'in'  'not in' # Usados para verificar a presença de um valor em uma sequência.
+# Strings são iteráveis em Python, o que significa que podemos 
+# verificar a presença de caracteres ou substrings dentro de uma
+# string usando os operadores 'in' e 'not in'.
+# 0 1 2 3 # Índices
+# H a r y # String
+# -6 -5 -4 -3 -2 -1 # Índices negativos
+nome = 'Hary'
+#print(nome[2]) # Acessando caractere por índice
+#print(nome[-4]) # Acessando caractere por índice negativo
+#print('y' in nome)  # Verifica se 'y' está em nome -> True  
+#print('F' in nome)  # Verifica se 'F' está em nome -> False
+#print('Har' in nome)  # Verifica se 'Har' está em nome -> True
+#print(10 * '-')  # Imprime uma linha de separação
+#print( 'Har' not in nome)  # Verifica se 'Har' não está em nome -> False
+#print( 'F' not in nome)  # Verifica se 'F' não está em nome -> True
+
+nome = input('Digite o seu nome: ')
+encontrar = input('Digite o que deseja encontrar: ')
+
+if encontrar in nome:
+    print(f'{encontrar} está em {nome}')
+else:
+    print(f'{encontrar} não está em {nome}')
