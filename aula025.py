f2fbc2a7f078292159e5399b95f33e750cec02c1
SEU_NOME <SEU_EMAIL>
2025-11-02 15:19:13 -0300

    Auto: atualiza aulas (2025-11-02 15:19:13)

---
+++ aula025.py
@@ -0,0 +1,17 @@
+a1 = 100
+a2 = 50
+a3 = 250
+a4 = 10
+
+print(min(a1, a2, a3, a4))
+print(10 * '-')
+print(max(a1, a2, a3, a4))
+print(10 * '-')  
+print(sum((a1, a2, a3, a4)))
+print(10 * '-')
+print(sorted((a1, a2, a3, a4))) 
+print(10 * '-')
+print((a1 + a2 + a3 + a4 ) / 4)
+print(10 * '-')
+print(round((a1 + a2 + a3 + a4 ) / 4,3))
+print(10 * '-')
