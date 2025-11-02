1bf7ad2e2bade9d9e734618c647581eda849b39a
SEU_NOME <SEU_EMAIL>
2025-11-02 13:30:31 -0300

    Auto: atualiza aulas (2025-11-02 13:30:31)

--- aula024.py
+++ aula024.py
@@ -22,3 +22,5 @@ if encontrar in nome:
     print(f'{encontrar} está em {nome}')
 else:
     print(f'{encontrar} não está em {nome}')
+
+# Teste de auto-sync: esta linha foi adicionada automaticamente
