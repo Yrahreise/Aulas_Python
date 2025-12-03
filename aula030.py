a348eee75d60a2d4306795d843420d6347671415
SEU_NOME <SEU_EMAIL>
2025-12-03 15:28:46 -0300

    Auto: atualiza aulas (2025-12-03 15:28:46)

--- aula030.py
+++ aula030.py
@@ -9,3 +9,5 @@ local_carro = 90  # local em que o carro está na estrada
 RADAR_1 = 60  # velocidade máxima do radar 1
 LOCAL_1 = 100  # local onde o radar 1 está
 RADAR_RANGE = 1  # A distância onde o radar pega
+
+if velocidade > RADAR_1:
