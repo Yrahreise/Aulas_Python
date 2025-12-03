e24a51705d842870091f164a9d2e8d85e8a5d804
SEU_NOME <SEU_EMAIL>
2025-12-03 15:17:36 -0300

    Auto: atualiza aulas (2025-12-03 15:17:36)

--- aula030.py
+++ aula030.py
@@ -4,7 +4,7 @@ Muitas condições no mesmo if (ruim)
     <- Contagem de complexidade (ruim)
 """
 velocidade = 61  # velocidade atual do carro
-local_carro = 100  # local em que o carro está na estrada
+local_carro = 90  # local em que o carro está na estrada
 
 RADAR_1 = 60  # velocidade máxima do radar 1
 LOCAL_1 = 100  # local onde o radar 1 está
