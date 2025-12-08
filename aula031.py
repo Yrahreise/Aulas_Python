01e669e70d2e5af87fad5252c95b1f60e6b526ab
SEU_NOME <SEU_EMAIL>
2025-12-08 18:31:15 -0300

    Auto: atualiza aulas (2025-12-08 18:31:14)

--- aula031.py
+++ aula031.py
@@ -5,11 +5,22 @@ is e is not = é ou não é(tipo,valor, identidade)
 id = identidade do objeto
 """
 
-v1 = 'a'
-print(id(v1))
-# condicao = false
+#v1 = 'a' # variável recebe o valor 'a'
+#v2 = 'b' # variável recebe o valor 'b'
+#print(id(v1)) # imprime o id (identidade) do objeto v1
+#print(id(v2)) # imprime o id (identidade) do objeto v2
 
-# if condicao:
-#     print('Faça algo')
-# else:
-#     print('Não faça algo')
+condicao = True
+passou_no_if = None
+
+if condicao:
+     passou_no_if = True
+     print('Faça algo') 
+else:
+     print('Não faça algo')
+
+
+if passou_no_if is None:
+     print('Não passou no if')
+else:
+     print('Passou no if')
