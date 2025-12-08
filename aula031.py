7392a6eb1542fd3324807e0b94a02d4b19401d12
SEU_NOME <SEU_EMAIL>
2025-12-08 16:44:09 -0300

    Auto: atualiza aulas (2025-12-08 16:44:09)

--- aula031.py
+++ aula031.py
@@ -1,6 +1,13 @@
 """
-Flag (Bandeira) - marcar um local
+Flag (Bandeira) - marcar um local no código
 None = Não valor
 is e is not = é ou não é(tipo,valor, identidade)
+id = identidade do objeto
+"""
 
- """
+# condicao = false
+
+# if condicao:
+#     print('Faça algo')
+# else:
+#     print('Não faça algo')
